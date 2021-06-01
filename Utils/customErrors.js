8f02b60200e2e143c5562ac2a4bf8759f27395a6
iamabhishek21 <abhishek@codeops.tech>
2021-06-01 18:55:09 +0530

    Add code coverage and Add more test cases

--- Utils/customErrors.js
+++ Utils/customErrors.js
@@ -1,15 +1,7 @@
-class SendEmailError extends Error {
-  constructor(errorDetails) {
-    super("Failed to send email");
-    this.errorDetails = errorDetails;
-  }
-}
-
 const DB_ERRORS = {
   DUPLICATE_KEY_ERROR: 1062
 };
 
 module.exports = {
-  DB_ERRORS,
-  SendEmailError
+  DB_ERRORS
 };
