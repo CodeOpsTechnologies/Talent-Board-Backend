24709b85b61dad2a180b66980ca774a0c2b61da2
Abhishek Rajendra Wagh <waghabhi21@gmail.com>
2021-06-16 18:20:11 +0530

    Main prod sync (#25)
    * minor changes
    
    * refering region from CLI command (#18)
    
    Merging PR
    
    * Add code coverage and Add more test cases (#20)
    
    Co-authored-by: iamabhishek21 <abhishek@codeops.tech>
    
    * Remove coverage dir from gitignore (#21)
    
    Co-authored-by: iamabhishek21 <abhishek@codeops.tech>
    
    * Fix: Profile expiry not happening as expected (#23)
    
    * Fix: Profile expiry not happening as expected
    
    Co-authored-by: iamabhishek21 <abhishek@codeops.tech>
    
    Co-authored-by: srushith <srushith.r@gmail.com>
    Co-authored-by: iamabhishek21 <abhishek@codeops.tech>

--- Utils/constants.js
+++ Utils/constants.js
@@ -3,6 +3,16 @@ const TalentBoardUserStatus = {
   EXPIRED: 2
 };
 
+// This is a mapping for visibility duration (the number of days, profile stays active)
+const VisibilityDurationMapping = {
+  1: 15,
+  2: 30,
+  3: 45,
+  4: 60,
+  5: 90
+};
+
 module.exports = {
-  TalentBoardUserStatus
+  TalentBoardUserStatus,
+  VisibilityDurationMapping
 };
